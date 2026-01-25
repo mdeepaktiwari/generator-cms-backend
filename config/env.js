028728aec3c1d97cd5c3596481f3fc7bd069d1fd
Deepak Tiwari <110996070+mdeepaktiwari@users.noreply.github.com>
2026-01-25 22:55:18 +0530

    fix: validate env

--- config/env.js
+++ config/env.js
@@ -1,13 +1,13 @@
 const validateEnvVariables = () => {
   const required = [
-    PORT,
-    MONGO_URL,
-    SECRET_KEY,
-    HUGGING_FACE_API_KEY,
-    CLOUDINARY_API_KEY,
-    CLOUDINARY_API_SECRET,
-    CLOUDINARY_CLOUD_NAME,
-    GEMINI_API_KEY,
+    "PORT",
+    "MONGO_URL",
+    "SECRET_KEY",
+    "HUGGING_FACE_API_KEY",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
+    "CLOUDINARY_CLOUD_NAME",
+    "GEMINI_API_KEY",
   ];
 
   const missingKeys = required.filter((key) => !process.env[key]);
@@ -16,3 +16,5 @@ const validateEnvVariables = () => {
     throw new Error(`Missing environment variable: ${missingKeys.join(" , ")}`);
   }
 };
+
+module.exports = validateEnvVariables;
